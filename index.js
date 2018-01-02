114dfe19c8eecbfbe4323a6c9bcc3dcb715321ce
Barney Carroll <barney.carroll@gmail.com>
2018-01-02 13:41:51 +0000

    API reference change, new delete API

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-function patch(target){
+function P(target){
   for(var i = 1; i < arguments.length; i++)
     for(var key in arguments[i])
       if(arguments[i].hasOwnProperty(key))
-        target[key] = (
-          arguments[i][key] instanceof scope
+        arguments[i][key] == D
+        ? delete target[key]
+        : target[key] =
+          arguments[i][key] instanceof S
           ? arguments[i][key].apply(target[key])
           : arguments[i][key]
-        )
 
   return target
 }
 
-function scope(closure){
-  if(!(this instanceof scope))
-    return new scope(closure)
+function S(closure){
+  if(!(this instanceof S))
+    return new S(closure)
 
   this.apply = function(definition){
     return closure(definition)
   }
 }
 
-function ps(target, input){
-  return arguments.length === 2
-  ? new scope(function(definition){
-    return patch(target, definition, input)
-  })
-  : new scope(function(definition){
-    return patch(definition, target) // `target` really is `input` in that case
-  })
+function PS(target, input){
+  return new S(
+    arguments.length === 2
+    ? function(definition){
+      return P(target, definition, input)
+    }
+    : function(definition){
+      return P(definition, target)
+    }
+  )
 }
 
+function D(){}
+
 try {
-  module.exports = {patch: patch, scope: scope, ps: ps}
+  module.exports = {P: P, S: S, PS: PS, D: D}
 } catch(e) {}
