fa16999eb3dbc8103a020312f05bd1dd81cd8b52
Barney Carroll <barney.carroll@gmail.com>
2018-04-28 16:09:45 +0100

    Remove overloaded API from explicit module (one or t'other)

--- explicit.js
+++ explicit.js
@@ -2,7 +2,7 @@ function P(target){
   for(var i = 1; i < arguments.length; i++)
     for(var key in arguments[i])
       if(arguments[i].hasOwnProperty(key))
-        (arguments[i][key] === D || arguments[i][key] === O)
+        (arguments[i][key] === D)
         ? delete target[key]
         : target[key] =
           arguments[i][key] instanceof S
